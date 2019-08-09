f6c0e61f2c69f86509d0e80f89167f2ccc27c6e3
Daniel Kamar <kamarster@gmail.com>
2019-08-09 16:03:45 +0300

    full stack app for education

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv/config');
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+
+
+// Server setting
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('./staticfiles'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'staticfiles', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Koitoror University frontend application listening on port(${port})`);
+});
