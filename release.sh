249601e07a07c61ba5a0b475d40d21d5a37b7e62
Daniel Kamar <kamarster@gmail.com>
2019-08-09 17:01:28 +0300

    full stack app for education

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 echo "Running Release Tasks"
 
 echo "Running Database migrations and migrating the new changes"
-python manage.py migrate api, users --noinput
-# python manage.py migrate --noinput
+python manage.py makemigrations account admin api auth authtoken contenttypes sessions sites socialaccount users
+python manage.py migrate --noinput
 
 echo "Done.."
