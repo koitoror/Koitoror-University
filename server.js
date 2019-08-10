4493d57e77160596d6e34322435997367072cc63
Daniel Kamar <kamarster@gmail.com>
2019-08-10 18:58:18 +0300

    full stack app for education

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
-require('dotenv/config');
 const express = require('express');
-const http = require('http');
+const bodyParser = require('body-parser');
 const path = require('path');
+
 const app = express();
+const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Server setting
-// Serve static assets if in production
+// API calls
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('./build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
-const port = process.env.PORT || 3000;
-const server = http.createServer(app);
-
-server.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Koitoror University frontend application listening on port(${port})`);
-});
+app.listen(port, () => console.log(`Listening on port ${port}`));
