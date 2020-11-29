1f45c3cdb417808e169645d12851a149ae756faf
Daniel Kamar <kamarster@gmail.com>
2020-11-29 15:56:23 +0300

    bug(full-stack-fix): presentation and linting

--- backend/home/settings/prod.py
+++ backend/home/settings/prod.py
@@ -27,7 +27,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        # 'PORT': os.environ.get('DB_PORT'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
--- frontend/src/store/actions/auth.js
+++ frontend/src/store/actions/auth.js
@@ -2,9 +2,9 @@ import axios from "axios";
 import * as actionTypes from "./actionTypes";
 
 // Backend API URL
-// const uri = 'https://koitoror-university.herokuapp.com'
+const uri = 'https://koitoror-university.herokuapp.com'
 // const uri = 'http://127.0.0.1:8000'
-export const uri = 'http://127.0.0.1:8000'
+// export const uri = 'http://127.0.0.1:8000'
 
 export const authStart = () => {
   return {
