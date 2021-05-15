905588fb05f0e72b215f4e0c437da7f20deb4f58
Daniel Kamar <kamarster@gmail.com>
2021-05-15 14:15:40 +0300

    fix prod issues

--- backend/home/settings/base.py
+++ backend/home/settings/base.py
@@ -44,7 +44,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.home.urls'
