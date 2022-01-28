388d22cabe37b04609d3a19e082d22c572ad612a
Daniel Kamar <kamarster@gmail.com>
2022-01-28 14:09:28 +0300

    chore(restructure): move manage.py to backend folder

--- release.sh
+++ release.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
+
+# set -e
+
+# ls -la /vol/
+# ls -la /vol/web
+
+# whoami
+
 echo "Running Release Tasks"
 
+# python backend/manage.py wait_for_db
+# python backend/manage.py collectstatic --noinput
+
 echo "Running Database migrations and migrating the new changes"
-# python manage.py makemigrations account admin assignment auth authtoken contenttypes sessions sites socialaccount users
-python manage.py migrate --noinput
+# python backend/manage.py makemigrations account admin assignment auth authtoken contenttypes sessions sites socialaccount users
+# python backend/manage.py makemigrations
+python backend/manage.py migrate --noinput
 
 echo "Done.."
