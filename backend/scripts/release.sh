7e9b048b582592eeb8cadd323d218409ff95e0c8
Daniel Kamar <kamarster@gmail.com>
2022-01-28 14:24:02 +0300

    Update release tasks to point to the backend folder

---
+++ backend/scripts/release.sh
@@ -0,0 +1,20 @@
+# !/bin/sh
+
+set -e
+
+ls -la /vol/
+ls -la /vol/web
+
+whoami
+
+echo "Running Release Tasks"
+
+python backend/manage.py wait_for_db
+python backend/manage.py collectstatic --noinput
+
+echo "Running Database migrations and migrating the new changes"
+# python backend/manage.py makemigrations account admin assignment auth authtoken contenttypes sessions sites socialaccount users
+python backend/manage.py makemigrations
+python backend/manage.py migrate --noinput
+
+echo "Done.."
