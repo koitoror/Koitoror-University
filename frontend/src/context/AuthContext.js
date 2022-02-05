1cbeca9d3637a787430c7b5e00ee91ae4c69507f
Daniel Kamar <kamarster@gmail.com>
2022-02-05 20:34:31 +0300

    feat(auth): add context-api provider

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,107 +1,132 @@
 import React, { createContext, useState, useEffect } from 'react'
-import jwt_decode from "jwt-decode";
+import jwt_decode from 'jwt-decode'
 import { useNavigate } from 'react-router-dom'
 
-const AuthContext = createContext('')
+const AuthContext = createContext();
 
 // Backend API URL
-import { uri } from '../store/actions/url';
+import { API_HOST as uri } from '../store/actions/api';
 
-export default AuthContext;
+export default AuthContext
 
+export const AuthProvider = ({ children }) => {
+    
+    const [isPanelRightActive, setIsPanelRightActive] = useState(false);
 
-export const AuthProvider = ({children}) => {
-    let [authTokens, setAuthTokens] = useState(()=> localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null)
-    let [user, setUser] = useState(()=> localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null)
+    const handleClickSignIn = () => {
+        setIsPanelRightActive(false)
+    }
+
+    const handleClickSignUp = () => {
+        setIsPanelRightActive(true)
+    }
+
+    const switchMode = () => {
+        setIsPanelRightActive(!isPanelRightActive)
+    }
+
+    let [authTokens, setAuthTokens] = useState(() =>
+        localStorage.getItem('authTokens')
+            ? JSON.parse(localStorage.getItem('authTokens'))
+            : null,
+    )
+    let [user, setUser] = useState(() =>
+        localStorage.getItem('authTokens')
+            ? jwt_decode(localStorage.getItem('authTokens'))
+            : null,
+    )
     let [loading, setLoading] = useState(true)
 
-    // const history = useHistory()
-    const history = useNavigate()
+    const navigate = useNavigate()
 
-    let loginUser = async (e )=> {
+    let loginUser = async (e) => {
         e.preventDefault()
 
         let response = await fetch(`${uri}/api/token/`, {
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
             },
-            body:JSON.stringify({'username':e.target.username.value, 'password':e.target.password.value})
+            body: JSON.stringify({
+                username: e.target.username.value,
+                password: e.target.password.value,
+            }),
         })
+        console.log('RESPONSE', response)
         let data = await response.json()
 
-        if(response.status === 200){
+        if (response.status === 200) {
             setAuthTokens(data)
             setUser(jwt_decode(data.access))
             localStorage.setItem('authTokens', JSON.stringify(data))
-            history('/')
-        }else{
+            navigate('/home2')
+        } else {
             alert('Something went wrong!')
         }
     }
-
 
     let logoutUser = () => {
         setAuthTokens(null)
         setUser(null)
         localStorage.removeItem('authTokens')
-        history('/login')
+        // navigate('/login2')
+        navigate('/auth')
     }
 
-
-    let updateToken = async ()=> {
-
+    let updateToken = async () => {
         let response = await fetch(`${uri}/api/token/refresh/`, {
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
             },
-            // body:JSON.stringify({'refresh':authTokens?.refresh})
-            body:JSON.stringify({'refresh':(authTokens && authTokens.refresh) })
+            body:JSON.stringify({'refresh':authTokens?.refresh})
+            // body: JSON.stringify({ refresh: authTokens && authTokens.refresh }),
         })
 
         let data = await response.json()
-        
-        if (response.status === 200){
+
+        if (response.status === 200) {
             setAuthTokens(data)
             setUser(jwt_decode(data.access))
             localStorage.setItem('authTokens', JSON.stringify(data))
-        }else{
+        } else {
             logoutUser()
         }
 
-        if(loading){
+        if (loading) {
             setLoading(false)
         }
     }
 
     let contextData = {
-        user:user,
-        authTokens:authTokens,
-        loginUser:loginUser,
-        logoutUser:logoutUser,
+        user: user,
+        authTokens: authTokens,
+        loginUser: loginUser,
+        logoutUser: logoutUser,
+        handleClickSignIn,
+        handleClickSignUp,
+        switchMode,
+        setIsPanelRightActive,
+        isPanelRightActive
     }
 
-
-    useEffect(()=> {
-
-        if(loading){
+    useEffect(() => {
+        if (loading) {
             updateToken()
         }
 
         let fourMinutes = 1000 * 60 * 4
 
-        let interval =  setInterval(()=> {
-            if(authTokens){
+        let interval = setInterval(() => {
+            if (authTokens) {
                 updateToken()
             }
         }, fourMinutes)
-        return ()=> clearInterval(interval)
-
+        return () => clearInterval(interval)
     }, [authTokens, loading])
 
-    return(
-        <AuthContext.Provider value={contextData} >
+    return (
+        <AuthContext.Provider value={contextData}>
             {loading ? null : children}
         </AuthContext.Provider>
     )
