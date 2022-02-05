301aa3acc4c1faf66cf5e9c8ad1aad6be698816e
Daniel Kamar <kamarster@gmail.com>
2022-02-05 20:37:01 +0300

    feat(auth): provide from context-api

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,37 +1,56 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useRoutes, Navigate, } from "react-router-dom";
+
+// local Imports
 import Hoc from "./hoc/hoc";
 
-import Login from "./containers/Login";
+import { AuthProvider } from './context/AuthContext'
+import PrivateRoute, { PrivateWrapper } from './utils/PrivateRoute'
+
+import Auth from './pages/Auth';
 import Signup from "./containers/Signup";
+import Login from "./containers/Login";
+import LoginPage from './pages/LoginPage'
 import Profile from "./containers/Profile";
+
+import Header from './components/Header'
+import Home from './pages/Home';
+import HomePage from './pages/HomePage'
 import AssignmentList from "./containers/AssignmentList";
 import AssignmentDetail from "./containers/AssignmentDetail";
 import AssignmentCreate from "./containers/AssignmentCreate";
 
-// import PrivateRoute from './utils/PrivateRoute'
-// import { AuthProvider } from './context/AuthContext'
-
-// import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-// import Header from './components/Header'
 
 const BaseRouter = () => (
   <Hoc>
-    {/* <AuthProvider> */}
+    <AuthProvider>
       {/* <Header/> */}
       <Routes>
+
+        // AUTH ROUTES
+        {/* <Route exact path="/auth/" element={<Auth/>} /> */}
+        <Route exact path="/login/" element={<Auth/>} />
+        <Route exact path="/signup/" element={<Signup/>} />
+        <Route exact path="/auth/" element={<Login/>} />
+        {/* <Route exact path="/login/" element={<Login/>} /> */}
+        <Route exact path="/login2" element={<LoginPage/>}/>
+        <Route exact path="/profile/:user_id" element={<Profile/>} />
+
+        // ASSIGNMENTS ROUTES
         <Route exact path="/" element={<AssignmentList/>} />
         <Route exact path="/create/" element={<AssignmentCreate/>} />
-        <Route exact path="/login/" element={<Login/>} />
-        <Route exact path="/login2" element={<LoginPage/>}/>
-        <Route exact path="/signup/" element={<Signup/>} />
         <Route exact path="/assignments/:id" element={<AssignmentDetail/>} />
-        <Route exact path="/profile/:id" element={<Profile/>} />
+
+        // PROTECTED ROUTES
+        {/* <Route exact path='/home2' element={<PrivateRoute component={HomePage}/>}/> */}
+        <Route element={<PrivateWrapper />}>
+          <Route path="/home1" element={<Home />} />
+          <Route path="/home2" element={<HomePage />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Route>
 
       </Routes>
-      {/* <PrivateRoute exact path="/home" element={HomePage}/> */}
-    {/* </AuthProvider> */}
+    </AuthProvider>
   </Hoc>
 );
 
