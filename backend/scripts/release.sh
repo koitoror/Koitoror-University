a8d06fab1cb5a3899eaca0f0f86e38dd1a00de21
Daniel Kamar <kamarster@gmail.com>
2022-02-22 06:54:23 +0300

    fix(prod-migrations-profiles):  add all fields

--- backend/scripts/release.sh
+++ backend/scripts/release.sh
@@ -14,7 +14,8 @@ echo "Running Release Tasks"
 
 echo "Running Database migrations and migrating the new changes"
 # python backend/manage.py makemigrations account admin assignment auth authtoken contenttypes sessions sites socialaccount profiles
-python backend/manage.py makemigrations
+python backend/manage.py makemigrations account admin assignment auth authtoken base contenttypes core profiles sessions sites socialaccount token_blacklist
+python backend/manage.py makemigrations 
 python backend/manage.py migrate --noinput
 
 echo "Done.."
