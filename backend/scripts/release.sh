d4f2212037376b17fd90ce72025ab20ad5f73eb8
Daniel Kamar <kamarster@gmail.com>
2022-03-05 19:11:46 +0300

    bug(fix): release

--- backend/scripts/release.sh
+++ backend/scripts/release.sh
@@ -16,6 +16,7 @@ echo "Running Database migrations and migrating the new changes"
 # python backend/manage.py makemigrations account admin assignment auth authtoken contenttypes sessions sites socialaccount profiles
 python backend/manage.py makemigrations account admin assignment auth authtoken base contenttypes core profiles sessions sites socialaccount token_blacklist
 # python backend/manage.py makemigrations
+python backend/manage.py migrate --plan
 python backend/manage.py migrate --noinput
 
 echo "Done.."
